93093ef80fdfbac6528b4933627a6ccf1d622b21
PE LETT <paulemmanuel.lett@gmail.com>
2020-01-19 11:27:44 +0100

    Fix new title update method

--- script.js
+++ script.js
@@ -12,29 +12,22 @@ function addTask()
 		.forEach(e => e.name = taskLabel);
   clone.querySelector("#del_button")
 		.onclick = () => removeTask(taskLabel);
-  let tti = clone.querySelector("#task_title_input")
-  let ttn = clone.querySelector(".touchtaskname");
+  
+  let tti = clone.querySelector("#task_title_input");
   tti.name = taskLabel;
-  tti.addEventListener('blur', () => { ttn.innerText=tti.value;});
-  tti.addEventListener('keypress', (event) => {
+  
+  tti.addEventListener('blur', () => {
+    console.log(tti);
+   tti.parentNode.parentNode.parentNode.children[0].textContent = tti.value ;
+   });
+  tti.addEventListener('keypress', (event) => { 
     if(event.keyCode==13){event.srcElement.blur();};
   });
-  
   
   document.querySelector("#tasklist")
 		.appendChild(taskContainer)
 			.appendChild(clone);
 }
 function removeTask(taskName) { document.querySelector("#"+taskName).remove();}
-
-
-function update_title()
-{
-}
-function unfocus()
-{
-	console.log(event);
-	if(event.keyCode==13){document.getElementById("task_title_input").blur();}
-}
 
 window.onload = ()=>{addTask()};
